7db1ec2e9b6982c77d307e9610b446815b828806
matheus17112006 <105861336+matheus17112006@users.noreply.github.com>
2023-09-11 09:01:52 -0300

    a

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ function tocaSom (idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas=document.querySelectorAll(''.tecla');
+const listaDeTeclas=document.querySelectorAll('.tecla');
 
 //para
 for(let contador=0;contador<listaDeTeclas.lenght;contador++){
     
     const tecla=listaDeTeclas[contador];
     const instrumento=tecla.classList[1];
-    const idAudio="#som_${instrumento}";//templante string
+    const idAudio='#som_${instrumento}';//templante string
 
-    tecla.onclick=function()[
+    tecla.onclick=function(){
         tocaSom(idAudio);
-    ]
+    }a
 }
