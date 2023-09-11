846a46fa6edc8d9aefdb7365324d016f9f03ef47
matheus17112006 <105861336+matheus17112006@users.noreply.github.com>
2023-09-11 09:04:19 -0300

    a

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ for(let contador=0;contador<listaDeTeclas.lenght;contador++){
     
     const tecla=listaDeTeclas[contador];
     const instrumento=tecla.classList[1];
-    const idAudio='#som_${instrumento}';//templante string
+    const idAudio=`#som_${instrumento}`;//templante string
 
     tecla.onclick=function(){
         tocaSom(idAudio);
